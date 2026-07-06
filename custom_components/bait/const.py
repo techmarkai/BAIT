2b922c0f4429e4dd993f9a7db0c5416d7ea583a3
techmarkai <techmark2026@gmail.com>
2026-07-06 09:16:46 +0300

    v0.2.0: relay constants (RELAY_URL, instance creds)

--- custom_components/bait/const.py
+++ custom_components/bait/const.py
@@ -5,10 +5,15 @@ DOMAIN = "bait"
 STORAGE_KEY = "bait_devices"
 STORAGE_VERSION = 1
 
-# Config entry data
-CONF_SERVICE_ACCOUNT = "service_account"
+# Config entry data (per-install relay credentials)
+CONF_INSTANCE_ID = "instance_id"
+CONF_INSTANCE_KEY = "instance_key"
 
-# HTTP endpoints
+# Central BAIT push relay (Firebase Cloud Functions in project bait-632d4).
+# Update if the deployed region/URL differs.
+RELAY_URL = "https://us-central1-bait-632d4.cloudfunctions.net"
+
+# HTTP endpoints (phone -> HA)
 REGISTER_URL = "/api/bait/register"
 UNREGISTER_URL = "/api/bait/unregister"
 
